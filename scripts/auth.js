34d8562dcf4f5c591bf2f326ab9f9ffba05a687a
BuildTools <bmceachnie@student.tgm.ac.at>
2021-11-23 00:54:23 +0100

    Update auth.js

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,5 +1,12 @@
-code = window.location.href.split("?code=")[1];
+const codeKeyword = "?code=";
+const paramSplitter = "&";
+const url = window.location.href;
+
+var code = "";
+
+if (url.includes(paramSplitter) && url.includes(codeKeyword)) code = url.substring(url.indexOf(codeKeyword) + codeKeyword.length, url.lastIndexOf(paramSplitter));
+else if (url.includes(codeKeyword)) code = url.substring(url.indexOf(codeKeyword) + codeKeyword.length);
 
 function openCodeBook() {
-  window.location.assign(`codebook://?code=${code}`);
+  window.location.assign(`codebook://${codeKeyword}${code}`);
 }
