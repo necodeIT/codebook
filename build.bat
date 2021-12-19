930a37967fb5cce15b5d0d35f628df41779a98d2
BuildTools <bmceachnie@student.tgm.ac.at>
2021-12-19 20:13:07 +0100

    Create build.bat

---
+++ build.bat
@@ -0,0 +1,12 @@
+call git pull --recurse-submodules
+cd nekolib 
+call git reset --hard origin/flutter
+cd ../web
+call flutter pub get
+call flutter build web --release --dart-define=FLUTTER_WEB_USE_SKIA=true --base-href="/codebook/"
+cd build/web
+call xcopy *.* "../../../docs" /i /s /y
+cd ../../../
+call git add docs/*.*
+call git commit -m "new web build"
+call git push
