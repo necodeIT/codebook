813621514e865f41893f83cea2a40e600fb6b0ca
bmceachnie <60017181+bmceachnie@users.noreply.github.com>
2022-03-11 19:43:41 +0100

    Delete installers directory

--- installers/Windows/build.bat
+++
@@ -1,7 +0,0 @@
-cd ..\codebook
-call flutter pub get
-call flutter build windows
-cd "..\url protocol handler"
-call dotnet publish -c Release --self-contained true -r win-x86
-cd ..\installers\Windows
-call iscc windows_setup.iss
